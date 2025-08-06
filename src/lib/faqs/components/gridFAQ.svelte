ef2dd7651766f1e25c85f0cf40c29a76c85b54df
Cristhian Agamez <cristhian_agamez@outlook.com>
2025-08-05 21:35:56 -0500

    fix: update footer link to point to FAQs and correct section heading in gridFAQ

--- src/lib/faqs/components/gridFAQ.svelte
+++ src/lib/faqs/components/gridFAQ.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <section class=" bg-graybrand shadow-2xl min-w-3xl min-h-96 rounded-2xl px-8 py-8">
-    <h3 class=" text-graybrand-light font-light">Categorías de Ayuda</h3>
+    <h3 class=" text-graybrand-light font-light">Preguntas de ayuda</h3>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {#each faqs as faq}
             <GridCard
--- src/lib/shared/components/Footer.svelte
+++ src/lib/shared/components/Footer.svelte
@@ -1,6 +1,11 @@
+<script lang="ts">
+	import TransitionLink from "./TransitionLink.svelte";
+</script>
 <footer class="mt-16 border-t border-[#D5D5D5]">
 	<div class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+		
 		<div class="text-center text-gray-600">
+			<TransitionLink href="/faqs">FAQS</TransitionLink>
 			<p class="text-sm">Copyright © 2025. All Rights Reseved.</p>
 		</div>
 	</div>
